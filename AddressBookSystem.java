894e42b1d651c33d481951f2b9a8211f536901ce
ppgupta <premprakashnayak69@gmail.com>
2020-09-30 02:01:28 +0530

    Contact added

--- AddressBookSystem.java
+++ AddressBookSystem.java
@@ -1,32 +1,50 @@
 package javapractice;
 
+import java.util.*;
 public class AddressBookSystem {
-	public class Contact{
-		private String firstName;
-		private String LastName;
-		private String address;
-		private String city;
-		private String state;
-		private int zip;
-		private int phoneNum;
-		private String email;
-		public Contact(String FN,String LN,String address,String city,String state,int zip,
-				int phone,String mail) {
-			this.firstName=FN;
-			this.LastName=LN;
-			this.address=address;
-			this.city=city;
-			this.state=state;
-			this.zip=zip;
-			this.phoneNum=phone;
-			this.email=mail;
+		private  ArrayList<String>ContactList1;
+
+		public void setContact1(ArrayList<String>Contact1) {
+			this.ContactList1=Contact1;
 		}
-	}
-	
+				public ArrayList<String> getContactList1(){
+			return this.ContactList1;
+		}
+		
 public static void main(String[] args) {
-System.out.println("Welcome to the address book program");
+	Scanner sc=new Scanner(System.in);
+	System.out.println("Enter FirstName");
+	String FN=sc.nextLine();
+	System.out.println("Enter LastName");
+	String LN=sc.nextLine();
+	System.out.println("Enter Address");
+	String address=sc.nextLine();
+	System.out.println("Enter City");
+	String city=sc.nextLine();
+	System.out.println("Enter State");
+	String state=sc.nextLine();
+	System.out.println("Enter Email");
+	String email=sc.nextLine();
+	System.out.println("Enter Zip");
+	int zip=sc.nextInt();
+	System.out.println("Enter Phone");
+	int phone=sc.nextInt();
+   String Zip=Integer.toString(zip);
+   String Phone=Integer.toString(phone);
+   
+	AddressBookSystem contact=new AddressBookSystem();
+	ArrayList<String>List1=new ArrayList<>();
 
+    List1.add(FN);
+    List1.add(LN);
+    List1.add(address);
+    List1.add(city);
+    List1.add(state);
+    List1.add(email);
+    List1.add(Zip);
+    List1.add(Phone);
+    contact.setContact1(List1);
 
+System.out.println("Welcome to the address book program "+List1);
 }
 }
-
